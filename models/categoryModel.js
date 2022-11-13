aa58a24f6a9b1d27d01b95f93572bb1af600ca02
Victor Raul Medina Meza <vrmm1998@gmail.com>
2022-11-13 17:12:06 -0500

    category model and route updated

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -2,11 +2,8 @@ const mongoose = require("mongoose");
 //const ObjectId = require("mongodb").ObjectId;
 
 const categoryModel = new mongoose.Schema({
-  name: { type: String, required: true }, //name displayed on screen
-  username: { type: String, required: true }, //unique username
-  location: { type: String, default: "" }, //where the user lives
-  description: { type: String, default: "" }, //a brief description of the user
-  date: { type: Date, default: Date.now }, //the date of creation of the user
+  category: { type: String, required: true }, //category name displayed on screen
+  date: { type: Date, default: Date.now }, //the date of creation of the category
 });
 
 module.exports = mongoose.model("Category", categoryModel);
