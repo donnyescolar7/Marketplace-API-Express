d5ec549d5572669e18eb967976ffb936ff06315f
donnyescolar7 <donnyescolargaviria@gmail.com>
2022-11-19 10:21:37 -0500

    done

--- models/productModel.js
+++ models/productModel.js
@@ -3,13 +3,11 @@ const ObjectId = require("mongodb").ObjectId;
 
 const productModel = new mongoose.Schema({
   description: { type: String, required: true }, //name displayed on screen
-  stars: { type: Number, default: 0}, //unique username
-  reviews: { type: [ObjectId], default: [] }, //where the user lives
   price: { type: Number, required: true }, //a brief description of the user
   brand: { type: String, required: true }, //name displayed on screen
   specs: { type: [String], required: true }, //list of product's specs
-  category: { type: ObjectId, required: true }, //where the user lives
-  date: { type: Date, default: Date.now }, //the date of creation of the user
+  category: { type: ObjectId, required: true }, //category of the product
+  date: { type: Date, default: Date.now }, //the date of creation of the product
 });
 
 module.exports = mongoose.model("Product", productModel);
